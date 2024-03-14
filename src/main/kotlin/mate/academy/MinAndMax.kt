549fb258380b422c6288b0e8458ca894e1adc904
Volodymyr Ozemblovskyi <Vladimir.ozem.ps@gmail.com>
2024-03-14 20:13:04 +0200

    solved task from Readme file: - increased jvmToolchain version up to 17 - implemented getMinAndMax() fun

--- src/main/kotlin/mate/academy/MinAndMax.kt
+++ src/main/kotlin/mate/academy/MinAndMax.kt
@@ -1,5 +1,8 @@
 package mate.academy
 
 fun getMinAndMax(inputArray: IntArray): IntArray {
-
+    var max: Int = inputArray[0]
+    var min: Int = inputArray[0]
+    inputArray.forEach { if (it > max) max = it else if (it < min) min = it }
+    return intArrayOf(min, max)
 }
