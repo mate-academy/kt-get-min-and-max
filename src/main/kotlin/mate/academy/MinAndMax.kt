7772bad2360bae91efe280718bcad7ce79cc586f
Andriy Beha <apgroup.ua@gmail.com>
2024-04-20 11:01:49 +0200

    Task completed

--- src/main/kotlin/mate/academy/MinAndMax.kt
+++ src/main/kotlin/mate/academy/MinAndMax.kt
@@ -1,5 +1,7 @@
 package mate.academy
 
 fun getMinAndMax(inputArray: IntArray): IntArray {
-
+    val minValue = inputArray.min()
+    val maxValue = inputArray.max()
+    return intArrayOf(minValue, maxValue)
 }
