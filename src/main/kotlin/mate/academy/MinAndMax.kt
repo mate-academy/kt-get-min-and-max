071c4049afde39344f76118857a2c867e20b54de
Danylo Borysenko <borysenkodanyl@gmail.com>
2024-05-15 18:20:53 +0300

    first commit

--- src/main/kotlin/mate/academy/MinAndMax.kt
+++ src/main/kotlin/mate/academy/MinAndMax.kt
@@ -1,5 +1,12 @@
 package mate.academy
 
-fun getMinAndMax(inputArray: IntArray): IntArray {
+const val ZERO_INDEX = 0
+const val FIRST_INDEX = 1
+const val ARRAY_SIZE = 2
 
+fun getMinAndMax(inputArray: IntArray): IntArray {
+    val result = IntArray(ARRAY_SIZE)
+    result[ZERO_INDEX] = inputArray.min()
+    result[FIRST_INDEX] = inputArray.max()
+    return result
 }
