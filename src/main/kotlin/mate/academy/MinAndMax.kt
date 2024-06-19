f2ec7cac6da15df87f72051a33902a23bc744fad
reamfox <sashafarion@gmail.com>
2024-06-19 11:39:33 +0300

    add solution

--- src/main/kotlin/mate/academy/MinAndMax.kt
+++ src/main/kotlin/mate/academy/MinAndMax.kt
@@ -1,5 +1,6 @@
 package mate.academy
 
 fun getMinAndMax(inputArray: IntArray): IntArray {
-
+    val sortedArray = inputArray.sortedArray()
+    return arrayOf(sortedArray.first(), sortedArray.last()).toIntArray()
 }
