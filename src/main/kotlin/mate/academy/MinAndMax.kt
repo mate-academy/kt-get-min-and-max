cd160be3f953ab65202f352f3e4ebf3224ba2567
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-06-29 09:56:31 +0300

    added hw

--- src/main/kotlin/mate/academy/MinAndMax.kt
+++ src/main/kotlin/mate/academy/MinAndMax.kt
@@ -1,5 +1,27 @@
 package mate.academy
 
 fun getMinAndMax(inputArray: IntArray): IntArray {
+    val min = getMin(inputArray)
+    val max = getMax(inputArray)
+    val result = intArrayOf(min, max)
+    return result
+}
 
+fun getMax(inputArray: IntArray): Int {
+    var max = Int.MIN_VALUE
+    inputArray.forEach { i ->
+        if (i > max) {
+            max = i
+    }
+    }
+    return max
+}
+fun getMin(inputArray: IntArray): Int {
+    var min = Int.MAX_VALUE
+    inputArray.forEach { i ->
+        if (i < min) {
+        min = i
+    }
+    }
+    return min
 }
