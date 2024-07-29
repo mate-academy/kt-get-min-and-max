ee2673aa87bb016c57dfe21a57521083560c02f3
Ruslan Cherniavskyi <ruslan.od.ua@ukr.net>
2024-07-29 14:06:27 +0300

    added code

--- src/main/kotlin/mate/academy/MinAndMax.kt
+++ src/main/kotlin/mate/academy/MinAndMax.kt
@@ -1,5 +1,11 @@
 package mate.academy
 
 fun getMinAndMax(inputArray: IntArray): IntArray {
+    if (inputArray.isEmpty()) {
+        throw IllegalArgumentException("Input array cannot be empty")
+    }
+    val min = inputArray.minOrNull() ?: throw IllegalStateException("Unexpected error: minOrNull returned null")
+    val max = inputArray.maxOrNull() ?: throw IllegalStateException("Unexpected error: maxOrNull returned null")
 
+    return intArrayOf(min, max)
 }
