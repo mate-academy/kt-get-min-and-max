622cb613d7a2ee9a1bf1e5c2a69e9cf81ef33319
oleksii.b <oleksii.b@dev.tagsoft.co>
2024-09-02 15:41:31 +0300

    solved task

--- src/main/kotlin/mate/academy/MinAndMax.kt
+++ src/main/kotlin/mate/academy/MinAndMax.kt
@@ -1,5 +1,8 @@
 package mate.academy
 
-fun getMinAndMax(inputArray: IntArray): IntArray {
+private const val FIRST_INDEX = 0
 
+fun getMinAndMax(inputArray: IntArray): IntArray {
+    val sortedArray = inputArray.sortedArray()
+    return intArrayOf(sortedArray[FIRST_INDEX], sortedArray[sortedArray.lastIndex])
 }
