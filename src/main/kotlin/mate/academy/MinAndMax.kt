dc4a40e7542dda47abf3b7949ece1371c5966b17
kraievskyi-om <kraievskyi-om@ukrposhta.com>
2024-12-02 12:07:04 +0200

    initial

--- src/main/kotlin/mate/academy/MinAndMax.kt
+++ src/main/kotlin/mate/academy/MinAndMax.kt
@@ -1,5 +1,7 @@
 package mate.academy
 
 fun getMinAndMax(inputArray: IntArray): IntArray {
-
+    return intArrayOf(
+        inputArray.minOrNull() ?: throw IllegalArgumentException("Array is empty"),
+        inputArray.maxOrNull() ?: throw IllegalArgumentException("Array is empty"))
 }
