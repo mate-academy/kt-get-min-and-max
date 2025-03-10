133465f973a5506a1b0141f4b877bb7096c5a870
Kateryna Mykhailovska <112807544+kateryna-mykh@users.noreply.github.com>
2025-03-11 01:13:17 +0200

    fix border range

--- src/main/kotlin/mate/academy/MinAndMax.kt
+++ src/main/kotlin/mate/academy/MinAndMax.kt
@@ -3,8 +3,8 @@ package mate.academy
 fun getMinAndMax(inputArray: IntArray): IntArray {
     val result: IntArray = IntArray(2)
     result[0] = inputArray[0] //min
-    result[1] = inputArray[1] //max
-    for (i in 1 until inputArray.size - 1) {
+    result[1] = inputArray[0] //max
+    for (i in 1 until inputArray.size) {
         if (inputArray[i] > result[1]) result[1] = inputArray[i]
         if (inputArray[i] < result[0]) result[0] = inputArray[i]
     }
