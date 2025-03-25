e60ba43fd55c25da031d95784d1b12726c65638b
Serhii Shyian <serhii.shn@gmail.com>
2025-03-25 13:22:21 +0200

    added min-max solution

--- src/main/kotlin/mate/academy/MinAndMax.kt
+++ src/main/kotlin/mate/academy/MinAndMax.kt
@@ -1,5 +1,8 @@
 package mate.academy
 
 fun getMinAndMax(inputArray: IntArray): IntArray {
+    val minValue = inputArray.minOrNull() ?: throw IllegalArgumentException("Array cannot be empty")
+    val maxValue = inputArray.maxOrNull() ?: throw IllegalArgumentException("Array cannot be empty")
 
+    return intArrayOf(minValue, maxValue)
 }
